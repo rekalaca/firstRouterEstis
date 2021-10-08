42149fb9bc612a313069a9da74b1df6ae1d4d903
rekalaca <rekalaca@gmail.com>
2021-10-08 18:36:18 +0200

    Az alap szerver elkészült

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+const port = 4444;
+
+
+const server = http.createServer( (req, res) => {
+    console.log(req.url);
+    console.log(req.method);
+});
+
+server.listen(port);
